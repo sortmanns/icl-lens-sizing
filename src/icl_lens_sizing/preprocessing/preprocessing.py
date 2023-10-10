6059cdcbdcf4b5e868f7102f72996eb2e203d255
sortmanns <sebastian.ortmanns@inovex.de>
2023-10-10 12:44:53 +0200

    organised experiments for regression

--- src/icl_lens_sizing/preprocessing/preprocessing.py
+++ src/icl_lens_sizing/preprocessing/preprocessing.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 
-def prepare_training_data(df, target, features=None, categorical_features=None, standardize=True, test_size=0.2,
-                          custom_features:dict=None):
+def prepare_training_data(df, target, features=None, categorical_features=None, custom_features:dict=None):
 
     # Drop artificial column
     df = df.drop(columns=["implantat_name","auge"])
@@ -20,7 +17,6 @@ def prepare_training_data(df, target, features=None, categorical_features=None, 
     target_df = df[target]
     feature_df = df.drop(columns=[target])
 
-
     if categorical_features:
         feature_df = pd.get_dummies(data=feature_df, columns=categorical_features, drop_first=True)
 
@@ -33,26 +29,49 @@ def prepare_training_data(df, target, features=None, categorical_features=None, 
     for i, col in enumerate(feature_df.columns):
         feature_mapping[i] = col
 
-    X_train, X_validation, y_train, y_validation = train_test_split(feature_df, target_df, test_size=test_size)
-    if standardize:
-        standart_scaler = StandardScaler()
-        X_train = standart_scaler.fit_transform(X_train)
-        X_validation = standart_scaler.fit_transform(X_validation)
-
-    return X_train, y_train, X_validation, y_validation, feature_mapping
+    return feature_df, target_df, feature_mapping
 
 
 def prepare_pred_data(df, features=None, categorical_features=None, custom_features:dict=None):
 
 
-    # Drop rows containing NaN's
-    df = df.dropna()
+    # try:
+    #     geschlecht = df["geschlecht"].iloc[0]
+    # except:
+    #     geschlecht = None
+    #
+    # if geschlecht:
+    #     if geschlecht=='w':
+    #         df['geschlecht_w']=1
+    #         df = df.drop(columns='geschlecht')
+    #     else:
+    #         df['geschlecht_w'] = 0
+    #         df = df.drop(columns='geschlecht')
+
 
     if categorical_features:
-        feature_df = pd.get_dummies(data=df, columns=categorical_features, drop_first=True)
+        feature_df = pd.get_dummies(data=df, columns=categorical_features, drop_first=False)
+    else:
+        feature_df = df
+
+    try:
+        feature_df["geschlecht_w"] = feature_df["geschlecht_m"]-1
+        feature_df = feature_df.drop(columns=['geschlecht_m'])
+    except:
+        pass
+
+    try:
+        feature_df["geschlecht_w"] = feature_df["geschlecht_div"]-1
+        feature_df = feature_df.drop(columns=['geschlecht_div'])
+    except:
+        pass
 
     if custom_features:
         for key, func in custom_features.items():
             feature_df[key] = feature_df.apply(func, axis=1)
+
+    if feature_df["alter"].isna().sum() > 0:
+        feature_df["alter"] = 40
+    feature_df = feature_df.drop(columns=['geschlecht'])
 
     return feature_df
--- src/icl_lens_sizing/run_script.py
+++ src/icl_lens_sizing/run_script.py
@@ -2,6 +2,9 @@ from preprocessing.preprocessing import prepare_training_data, prepare_pred_data
 import statsmodels.api as sm
 from sklearn.linear_model import Lasso
 import pandas as pd
+from datetime import datetime
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 from src.icl_lens_sizing.reporting.reports import create_named_data_frames, prepare_dfs_for_pred, \
     predict_ols_for_all_items, predict_for_all_items
@@ -14,16 +17,15 @@ def run_all(write: bool = True):
     df = pd.read_csv(path, sep=";", decimal=',')
 
     # prepare data
-    features = ['implantat_größe','geschlecht','alter', 'ACD', 'ACA_nasal', 'ACA_temporal', 'AtA', 'ACW',
-                'ARtAR_LR', 'StS', 'StS_LR', 'CBID', 'CBID_LR', 'mPupil', 'WtW_MS-39',
-                'WtW_IOL_Master', 'Sphaere', 'Zylinder', "Achse"]
+    features = ['implantat_size', 'AtA', 'ACW', 'ARtAR_LR', 'StS', 'CBID', 'WtW_MS-39', 'Sphaere']
+    custom_features = None # {'spherical_equivalent': (lambda row: row['Sphaere']-0.5*row['Zylinder'])}
 
-    custom_features = {'cbid_ratio': (lambda row: row['CBID'] / row['CBID_LR']),
-                       'spherical_equivalent': (lambda row: row['Sphaere']+0.5*row['Zylinder'])}
+    categoricals = None
 
-    categoricals = ['geschlecht']
+    standart_scaler = StandardScaler()
 
     bid = 1
+    predictions = pd.DataFrame()
     while bid <= df['befund_id'].max():
 
         if bid != 19:
@@ -38,33 +40,46 @@ def run_all(write: bool = True):
             bid=bid+1
             implantate = [[11.25, 11.50, 11.75, 12.00, 12.25, 12.50, 12.75, 13.00, 13.25, 13.50, 13.75]]
 
-        pred_df['implantat_größe'] = implantate
-        pred_df = pred_df.explode(column='implantat_größe')
+        pred_df['implantat_size'] = implantate
+        pred_df = pred_df.explode(column='implantat_size')
         pred_df = pred_df.drop(columns=["Lens-ICPL-Distance","implantat_name",'auge',"befund_id"])
         pred_df = prepare_pred_data(pred_df, features=features, categorical_features=categoricals,
                                     custom_features=custom_features)
 
 
-        X_train, y_train, X_validation, y_validation, feat_map = prepare_training_data(train_df , target="Lens-ICPL-Distance",
-                                                                                       features=features, test_size=0.2, categorical_features=categoricals,
-                                                                                       custom_features=custom_features, standardize=False)
+        feature_df, target_df, feat_map = prepare_training_data(train_df , target="Lens-ICPL-Distance",
+                                                                                       features=features, categorical_features=categoricals,
+                                                                                       custom_features=custom_features)
 
+        # standart_scaler.fit(feature_df)
+        # X_train = standart_scaler.transform(feature_df)
+        # X_predict = standart_scaler.transform(pred_df[['implantat_size', 'AtA', 'ACW', 'ARtAR_LR', 'StS', 'CBID', 'WtW_MS-39', 'Sphaere']])
         # train linear regression
         # X_ols = sm.add_constant(X_train, has_constant='add')
         # est = sm.OLS(y_train, X_ols).fit()
-
+        columns = list(feature_df.columns)
+        X_train_df = feature_df
+        X_predict_df = pred_df[['implantat_size', 'AtA', 'ACW', 'ARtAR_LR', 'StS', 'CBID', 'WtW_MS-39', 'Sphaere']]
         # train lasso regression
         model_lasso = Lasso()
-        model_lasso.fit(X_train, y_train)
+        model_lasso.fit(X_train_df, target_df)
 
-        preds_lasso = model_lasso.predict(pred_df)
+        preds_lasso = model_lasso.predict(X_predict_df)
         # preds_ols = est.predict(pred_df)
-        print(preds_lasso)
+        X_predict_df['prediction'] = preds_lasso
+        predictions = predictions.append(X_predict_df, ignore_index=False)
+
 
     if write:
-        preds_ols.to_csv("../../docs/predictions/preds_ols_2023_04_20.csv")
-        preds_lasso.to_csv("../../docs/predictions/preds_lasso_2023_04_20.csv")
+        # Get the current date
+        current_date = datetime.now()
+
+        # Create the date string with underscores as separators
+        date_string = current_date.strftime("%Y_%m_%d")
+
+        # preds_ols.to_csv(f"../../docs/predictions/preds_ols_{date_string}.csv")
+        predictions.to_csv(f"../../docs/predictions/preds_lasso_{date_string}.csv")
 
     return preds_lasso  #, preds_ols
 
-run_all(write=False)
+run_all(write=True)
